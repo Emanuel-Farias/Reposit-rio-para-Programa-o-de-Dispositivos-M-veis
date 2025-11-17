7df5482b8c173c9fdb148d708ba9f18289504b4d
Emanuel de Oliveira Farias <emanuel.oliveira.farias@gmail.com>
2025-11-17 08:16:24 -0300

    garantindo

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.app;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,17 +16,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
 
     Button b;
 
+    ListView listagem;
+
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextText);
+        b = findViewById(R.id.b);
+        listagem=findViewById(R.id.lista);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -34,20 +46,42 @@ public class MainActivity extends AppCompatActivity {
         db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT ," +
                 " titulo VARCHAR,texto TEXT)");
 
+        carregarListagem();
+
         b=findViewById(R.id.b);
         b.setOnClickListener(v -> {
-            EditText editText = findViewById(R.id.editTextText);
+
             String texto=editText.getText().toString();
             ContentValues cv = new ContentValues();
             cv.put("titulo","Nota de Exemplo");
             cv.put("texto", "Este é o texto da nota de exemplo");
             db.insert("notas", null,cv);
-            Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
 
         });
 
-
-
-
     }
+
+        public void carregarListagem(){
+            ArrayList<String> titulos = new ArrayList<String>();
+            Cursor cursor = db.rawQuery("SELECT * FROM notas",null);
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()){
+                String titulo = cursor.getString(cursor.getColumnIndex("titulo"));
+                titulos.add(titulo);
+                cursor.moveToNext();
+            }
+
+            ArrayAdapter<String> titulosAdapter = new ArrayAdapter<>(
+                    getApplication(),
+                    android.R.layout.simple_list_item_1,
+                    titulos
+            );
+        }
+
+
+
+
+
 }
